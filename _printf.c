b4260ce5deeac48f28994baecf6f56a7d25e30f2
raxidbou4nja <official.wetyn@gmail.com>
2023-07-23 20:50:45 -0700

    custom printf

--- _printf.c
+++ _printf.c
@@ -1,59 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
 #include "main.h"
 
-int print_char(char c) {
-    return write(1, &c, 1);
+/**
+ * _printf - prints a given string with identifiers
+ * @format:  format specifiers
+ * Return: length of pointer
+ */
+
+int _printf(const char *format, ...)
+{
+	int count;
+	va_list args;
+
+	va_start(args, format);
+	count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += handle_format_specifier(*format, args);
+
+		} else
+		{
+			count += print_char(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
-
-int print_string(const char *str) {
-    int count;
-    count = 0;
-    while (*str) {
-        count += print_char(*str);
-        str++;
-    }
-    return count;
-}
-
-int _printf(const char *format, ...) {
-  int count;
-    va_list args;
-    va_start(args, format);
-    
-    count = 0;
-
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c': {
-                    char c = (char)va_arg(args, int);
-                    count += print_char(c);
-                    break;
-                }
-                case 's': {
-                    const char *str = va_arg(args, const char *);
-                    count += print_string(str);
-                    break;
-                }
-                case '%': {
-                    count += print_char('%');
-                    break;
-                }
-                default:
-                    count += print_char('%');
-                    count += print_char(*format);
-                    break;
-            }
-        } else {
-            count += print_char(*format);
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
-}
-
