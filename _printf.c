268a7eccee5526de6bf95f728ea055c8a49e495f
raxidbou4nja <official.wetyn@gmail.com>
2023-07-23 19:53:50 -0700

    remove if condition

--- _printf.c
+++ _printf.c
@@ -22,10 +22,6 @@ int _printf(const char *format, ...)
 		return (-1);
 	}
 
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
-		return (-1);
-	}
 	while (*format)
 	{
 		if (*format == '%')
